143c1e69eabf652be71317a2ffc3f77b616f34f3
Zachary Goodman <znote89@gmail.com>
2021-08-30 12:36:53 -0400

    commit tree c6007c6a5494476e2fdee71e531a49245bcea118

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,18 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    if (params[:name]) 
+      student = Student.find_by(last_name: params[:name].capitalize())
+      students = []
+      students.push(student)
+    else students  = Student.all
+    end
     render json: students
+  end
+
+  def select
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
